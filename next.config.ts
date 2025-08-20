50cd2f794085f83deabceb86ec9f11bb2d0ff6aa
Devinda Dilshan <devindadilshan0@gmail.com>
2025-08-21 02:07:59 +0530

    add amplify auto building config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  trailingSlash: true,
   output: "export",
+  trailingSlash: true,
+  distDir: "out",
   images: {
-    unoptimized: false,
+    unoptimized: true,
   },
 };
 

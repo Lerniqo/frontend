429a719492a6b0e38edd7682a6354506bdb08d6c
Devinda Dilshan <devindadilshan0@gmail.com>
2025-09-01 02:42:40 +0530

    refactor the code

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useAuth } from '@/contexts/AuthContext'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const Protected = ({
+    teacher,
+    student,
+    admin,
+}:{
+    teacher: React.FC,
+    student: React.FC,
+    admin: React.FC,
+}) => {
+    const { user, isAuthenticated, isLoading } = useAuth()
+    const router = useRouter()
+
+    if (isLoading) return <div>Loading...</div>
+
+    if (!user || !isAuthenticated) return router.push('/login');
+
+    if (user.role === 'Teacher') return teacher;
+
+    if (user.role === 'Student') return student;
+
+    if (user.role === 'Admin') return admin;
+
+    throw new Error('Unknown user role');
+}
+
+export default Protected
